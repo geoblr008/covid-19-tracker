130dff4c8b863d2c4cc8984135e267de7cb35dd4
Rohith04MVK <007rohitjj04@gmail.com>
2022-02-17 19:54:57 +0530

    :sparkles: Added various data handlers

--- src/handler.py
+++ src/handler.py
@@ -1,40 +1,64 @@
+from operator import index
+from tracemalloc import start
 from bs4 import BeautifulSoup
 import requests
-import time
 
-country = "World"
-notification_duration = 10
-refresh_time = 10 #minutes
-data_check= []
+data_check = []
 worldmetersLink = "https://www.worldometers.info/coronavirus/"
+html_page = requests.get(worldmetersLink)
+bs = BeautifulSoup(html_page.content, 'html.parser')
+search = bs.select("div tbody tr td")
 
-def get_stats(array):
+
+def data_cleanup(array):
     L = []
     for i in array:
-        i = i.replace("+","")
-        i = i.replace("-","")
-        i = i.replace(",",".")
+        i = i.replace("+", "")
+        i = i.replace("-", "")
         if i == "":
             i = "0"
         L.append(i.strip())
     return L
-html_page = requests.get(worldmetersLink)
-bs = BeautifulSoup(html_page.content, 'html.parser')
 
-search = bs.select("div tbody tr td")
-start = -1
-for i in range(len(search)):
-    if search[i].get_text().find(country) !=-1:
-        start = i
-        break
-data = []
-for i in range(1,8):
-    try:
-        data = data + [search[start+i].get_text()]
-    except:
-        data = data + ["0"]
-    
-data= data_cleanup(data)
-message = "Total infected = {}, New Case = {}, Total Deaths = {}, New Deaths = {}, Recovred = {}, Active Case = {}, Serious Critical = {}".format(*data)
-print(message)
 
+def get_data(country):
+    start = -1
+    for i in range(len(search)):
+        if search[i].get_text().find(country) != -1:
+            start = i
+            break
+    data = []
+    for i in range(1, 8):
+        try:
+            data = data + [search[start+i].get_text()]
+        except:
+            data = data + ["0"]
+
+    data = data_cleanup(data)
+    keys = ["total_infected", "new_case", "total_deaths",
+            "new_deaths", "recovred", "active_case", "serious_critical"]
+
+    data = dict(zip(keys, data))
+
+    return data
+
+
+def world_data():
+    return get_data("World")
+
+
+def get_top_five():
+    index = [177, 199, 221, 243, 265]
+
+    data = []
+    for i in range(0, 8):
+        try:
+            data = data + [search[index[i]].get_text()]
+        except:
+            data = data + ["0"]
+        data = list(filter(("0").__ne__, data))
+    country_list = []
+    for i in range(len(data)):
+        country_list.append(get_data(data[i]))
+
+    return country_list
